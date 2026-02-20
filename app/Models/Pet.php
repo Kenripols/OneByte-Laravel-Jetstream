0e7b3bea463a175139977e91d2f51360191167af
Christian Lessa(Kenripols) <102255968+Kenripols@users.noreply.github.com>
2026-02-20 00:11:12 -0300

    Revert "Actualizaciones 16-12-25"
    This reverts commit bc2fcf6d4cdf7d1cd35d36c8ec813cf8fcedc399.

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -7,16 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-<<<<<<< Updated upstream
-use Spatie\Permission\Traits\HasRoles;
-=======
-//use Spatie\Permission\Traits\HasRoles; //lo sacaria
-use Illuminate\Database\Eloquent\Relations\HasOne; //16-12-25 Agregué HasOne
->>>>>>> Stashed changes
+use Spatie\Permission\Traits\HasRoles; //lo sacaria
 
 class Pet extends Model
 {
-    use HasFactory, SoftDeletes; //16-12-25 En esta linea saqué HasRoles
+    use HasFactory, HasRoles, SoftDeletes;
 
     protected $fillable = [
         'photo', 'name', 'bDate', 'breed_id', 'owner_id'
@@ -43,21 +38,11 @@ class Pet extends Model
     {
         return $this->hasMany(QRPlate::class, 'pet_id');
     }
-<<<<<<< Updated upstream
-=======
 
     // Especifico la relacion de una mascota tiene muchos historiales de estado
     public function pet_state_histories(): HasMany
     {
         return $this->hasMany(PetHistory::class, 'pet_id');
     }
-
-    // 16-12-25 Especifico la relacion de una mascota en un único historial, el mas reciente
-    public function currentState(): HasOne
-    {
-        return $this->hasOne(PetHistory::class, 'pet_id')
-                ->whereNull('endDate')
-                ->latest('beginDate');
-    }
->>>>>>> Stashed changes
 }
+//revisar el has roles que me parece que no va
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
-<<<<<<< Updated upstream
-=======
 use App\Http\Controllers\Admin\PetController;
-use App\Http\Controllers\Admin\DashboardController; // Agregado 16-12-25
->>>>>>> Stashed changes
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,21 +13,10 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-<<<<<<< Updated upstream
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
-=======
-    
-    // Inicio de agregado 16-12-25
-    Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard');
-    // Final de agregado 16-12-25
-
-    // Route::get('/dashboard', function () { //Comentado 16-12-25
-    //     return view('dashboard'); //Comentado 16-12-25
-    // })->name('dashboard'); //Comentado 16-12-25
 //Ver de mover las rutas admin a routes/admin.php
     // Rutas admin (solo usuarios con rol admin)
     Route::middleware('role:admin')->prefix('admin')->name('admin.')->group(function () {
@@ -43,27 +28,9 @@ Route::middleware([
         Route::get('users/trashed', [UserController::class, 'trashed'])->name('users.trashed');
         Route::patch('users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
         Route::resource('users', UserController::class);
->>>>>>> Stashed changes
 
-// Estas rutas deberian estar en routes/admin.php que es el archivo donde se guardan las rutas de admin
-// Rutas admin agrupadas con prefijo y nombre 'admin.'
-Route::prefix('admin')->name('admin.')->group(function () {
-
-    // Confirmación de borrado lógico
-    Route::get('users/{user}/confirm-delete', [UserController::class, 'confirmDelete'])->name('users.confirm-delete');
-
-    // Confirmación de borrado físico
-    Route::get('users/{user}/confirm-force-delete', [UserController::class, 'confirmForceDelete'])->name('users.confirm-force-delete');
-
-    // Borrado físico (force delete)
-    Route::delete('users/{user}/force-delete', [UserController::class, 'forceDelete'])->name('users.force-delete');
-    
-    Route::get('users/trashed', [UserController::class, 'trashed'])->name('users.trashed');
-    Route::patch('users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
-    
-    // CRUD completo para users
-    Route::resource('users', UserController::class);
-});
+        
+    });
 
     
 
