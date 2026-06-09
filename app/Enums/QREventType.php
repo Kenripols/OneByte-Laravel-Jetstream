d55ba3b6941526e683cf3752cb52e4a87837d716
Iván Rodríguez <raboymota@gmail.com>
2026-06-09 14:56:11 -0300

    COMMIT FLUJOS
    Quedaron hechos todos los flujos, (abiertos a testeo), el unico incompleto
    es el cuarto flujo, que hay que discutir el caso de estar logueado como admin

--- app/Enums/QREventType.php
+++ app/Enums/QREventType.php
@@ -6,9 +6,9 @@ enum QREventType: string
 {
     case GENERATED  = 'generated';  //1 Flujo 3 HECHO
     case DOWNLOADED = 'downloaded'; //2 Flujo 4 HECHO (no admin)
-    case CLAIMED    = 'claimed';    //3 Flujo 2 HECHO (no sesion iniciada)
+    case CLAIMED    = 'claimed';    //3 Flujo 2 HECHO
     //case REGISTERED = 'registered'; //
-    case ASSIGNED   = 'assigned';   //4 Flujo 1 
+    case ASSIGNED   = 'assigned';   //4 Flujo 1 HECHO
     case EXPIRED    = 'expired';    //5 Flujo 3 HECHO
     case REPLACED   = 'replaced';   //6 Flujo 3 HECHO    
 }
