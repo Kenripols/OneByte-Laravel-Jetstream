4d4ddfd240a299ddd8684f8fb401240917de0d09
Christian Lessa <ken.rip2@gmail.com>
2026-07-25 01:26:57 -0400

    Limpiando restos de merge

--- app/Enums/QrEventType.php
+++ app/Enums/QrEventType.php
@@ -8,14 +8,8 @@ enum QrEventType: string
     case DOWNLOADED = 'downloaded'; //2 Flujo 4 HECHO 
     case CLAIMED    = 'claimed';    //3 Flujo 2 HECHO
     //case REGISTERED = 'registered'; //
-<<<<<<< HEAD:app/Enums/QrEventType.php
-    case ASSIGNED   = 'assigned';   //4 Flujo 1 HECHO
-    case EXPIRED    = 'expired';    //5 Flujo 3 HECHO
-    case REPLACED   = 'replaced';   //6 Flujo 3 HECHO    
-}
-=======
     case ASSIGNED   = 'assigned';   //5 Flujo 1 HECHO
     case EXPIRED    = 'expired';    //6 Flujo 3 HECHO
     case REPLACED   = 'replaced';   //7 Flujo HECHO
 }
->>>>>>> origin/debug:app/Enums/QREventType.php
+
